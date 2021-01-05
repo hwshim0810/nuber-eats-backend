3205eb63e9f098d56a08657a2733fdcd0bfa9bfb
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-06 01:16:19 +0900

    Validating config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
+
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -11,6 +13,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       envFilePath:
         process.env.NODE_ENV === 'dev' ? '.env.development' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod', 'test')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
