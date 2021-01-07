8b6348df01b6618074475a213aca1f838c239008
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-08 01:27:37 +0900

    Create account with hash

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -9,5 +13,22 @@ export class UsersResolver {
   @Query(returns => Boolean)
   hello() {
     return true;
+  }
+
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+      if (error) {
+        return { ok: false, error };
+      }
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
