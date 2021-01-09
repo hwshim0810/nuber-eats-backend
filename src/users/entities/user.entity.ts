f2c52c161856da8d3a98fafb3c351145ccdba6e4
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-09 18:02:07 +0900

    Add login mutation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,6 +8,7 @@ import {
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { BeforeInsert, Column, Entity } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { IsEnum, IsString } from 'class-validator';
 
 enum UserRole {
   Owner,
@@ -23,20 +24,33 @@ registerEnumType(UserRole, { name: 'UserRole' });
 export class User extends CoreEntity {
   @Column()
   @Field(type => String)
+  @IsString()
   email: string;
 
   @Column()
   @Field(type => String)
+  @IsString()
   password: string;
 
   @Column({ type: 'enum', enum: UserRole })
   @Field(type => UserRole)
+  @IsEnum(UserRole)
   role: UserRole;
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
     try {
       this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async checkPassword(password: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(password, this.password);
+      return ok;
     } catch (e) {
       console.log(e);
       throw new InternalServerErrorException();
