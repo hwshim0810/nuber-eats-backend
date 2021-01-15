8f2e6829deaa45d3adcdb0e4a065f9fb5d9de4a4
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-15 20:15:49 +0900

    Fix mail module error

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,14 +1,15 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
   static forRoot(options: MailModuleOptions): DynamicModule {
     return {
       module: MailModule,
-      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, JwtService],
-      exports: [],
+      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, MailService],
+      exports: [MailService],
     };
   }
 }
