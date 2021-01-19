b246f5e438672bce18c1bca344206b7e262f4fca
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-19 17:13:28 +0900

    Add create account e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,12 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+const ENDPOINT = '/graphql';
+
+jest.mock('got', () => ({
+  post: jest.fn(),
+}));
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -21,10 +27,54 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('createAccount', () => {
+    const EMAIL = 'test@test.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "pass111",
+              role: Owner,
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "pass111",
+            role: Owner,
+          }) {
+            ok
+            error
+          }
+        }
+      `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
   });
 });
