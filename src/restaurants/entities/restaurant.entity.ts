7f1584147679cf823b6b72477c3b0bd7941f93ba
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-01-20 19:23:35 +0900

    Refine Restaurant, Category entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,37 +1,33 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
-export class Restaurant {
-  @Field(type => Number)
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class Restaurant extends CoreEntity {
   @Field(type => String)
   @Column()
   @IsString()
   name: string;
 
-  @Field(type => Boolean, { nullable: true })
-  @Column({ default: true })
-  @IsOptional()
-  @IsBoolean()
-  isVegan: boolean;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  coverImg: string;
 
   @Field(type => String)
   @Column()
   @IsString()
   address: string;
 
-  @Field(type => String)
-  @Column()
-  @IsString()
-  ownerName: string;
-
-  @Field(type => String)
-  @Column()
-  @IsString()
-  categoryName: string;
+  @Field(type => Category, { nullable: true })
+  @ManyToOne(
+    type => Category,
+    category => category.restaurants,
+    { nullable: true, onDelete: 'SET NULL' },
+  )
+  category: Category;
 }
