4abea120affa14c78fa7e97a48a4315736818e44
Hyunwoo Shim <shim.hw0810@gmail.com>
2021-12-26 20:42:08 +0900

    Refactor create restaurants

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,9 +1,16 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, ObjectType, OmitType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Restaurant } from '../entities/restaurant.entity';
 
+// #3.5 Mapped Types 강의참고
+// Mapped Type으로 받는 Entitiy가 InputType이 아니라면 아래와 같이 데코레이터 교체가 필요함
+// Restaurant entity 참조
 @InputType()
-export class CreateRestaurantDto extends OmitType(
+export class CreateRestaurantInput extends OmitType(
   Restaurant,
-  ['id'],
+  ['id', 'category', 'owner'],
   InputType,
 ) {}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {}
