2ad0e7d7d068d8fa5d432591671e298426785ed3
Hyunwoo Shim <shim.hw0810@gmail.com>
2022-01-08 23:32:45 +0900

    feat: Add dish resolver, dish option field

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.restaurant';
-import { CategoryResolver, RestaurantsResolver } from './restaurants.resolver';
+import {
+  CategoryResolver,
+  DishResolver,
+  RestaurantsResolver,
+} from './restaurants.resolver';
 import { RestaurantsService } from './restaurants.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-  providers: [RestaurantsResolver, CategoryResolver, RestaurantsService],
+  providers: [
+    RestaurantsResolver,
+    CategoryResolver,
+    DishResolver,
+    RestaurantsService,
+  ],
 })
 export class RestaurantsModule {}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -12,6 +12,7 @@ import { Role } from 'src/auth/role.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { AllCategoriesOutput } from './dtos/all-categories.dto';
 import { CategoryInput, CategoryOutput } from './dtos/category.dto';
+import { CreateDishInput, CreateDishOutput } from './dtos/create-dish.dto';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
@@ -31,6 +32,7 @@ import {
   SearchRestaurantOutput,
 } from './dtos/search-restaurant.dto';
 import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
 
@@ -104,5 +106,19 @@ export class CategoryResolver {
   @Query(type => CategoryOutput)
   category(@Args('input') input: CategoryInput): Promise<CategoryOutput> {
     return this.restaurantService.findCategoryBySlug(input);
+  }
+}
+
+@Resolver(of => Dish)
+export class DishResolver {
+  constructor(private readonly restaurantService: RestaurantsService) {}
+
+  @Mutation(type => CreateDishOutput)
+  @Role(['Owner'])
+  createDish(
+    @AuthUser() authUser: User,
+    @Args('input') input: CreateDishInput,
+  ): Promise<CreateDishOutput> {
+    return this.restaurantService.createDish(authUser, input);
   }
 }
