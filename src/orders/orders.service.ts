bc5fe2724152ad84461448bc3a5088bf4fcce34a
Hyunwoo Shim <shim.hw0810@gmail.com>
2022-05-29 22:26:58 +0900

    Find item option extra

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -27,10 +27,27 @@ export class OrderService {
     if (!restaurant) {
       return { ok: false, error: 'Restaurant not found' };
     }
-    items.forEach(async item => {
+    for (const item of items) {
       const dish = await this.dishes.findOne(item.dishId);
       if (!dish) {
         return { ok: false, error: 'Dish not found' };
+      }
+      for (const itemOption of item.options) {
+        const dishOption = dish.options.find(
+          dishOption => dishOption.name === itemOption.name,
+        );
+        if (dishOption) {
+          if (dishOption.extra) {
+            console.log(dishOption.extra);
+          } else {
+            const dishOptionChoice = dishOption.choices.find(
+              optionChoice => optionChoice.name === itemOption.choice,
+            );
+            if (dishOptionChoice.extra) {
+              console.log(dishOptionChoice.extra);
+            }
+          }
+        }
       }
       if (dish.restaurantId !== restaurantId) {
         return { ok: false, error: 'Dish is not from this restaurant' };
@@ -38,6 +55,6 @@ export class OrderService {
       await this.orderItems.save(
         this.orderItems.create({ dish, options: item.options }),
       );
-    });
+    }
   }
 }
