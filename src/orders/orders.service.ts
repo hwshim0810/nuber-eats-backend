49c7b47beda06e94e751b397e504cc12e5db11f0
Hyunwoo Shim <shim.hw0810@gmail.com>
2022-05-29 18:51:22 +0900

    Add orderItems repo

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order) private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Dish) private readonly dishes: Repository<Dish>,
   ) {}
 
   async createOrder(
@@ -22,11 +27,17 @@ export class OrderService {
     if (!restaurant) {
       return { ok: false, error: 'Restaurant not found' };
     }
-    const order = await this.orders.save(
-      this.orders.create({
-        customer,
-        restaurant,
-      }),
-    );
+    items.forEach(async item => {
+      const dish = await this.dishes.findOne(item.dishId);
+      if (!dish) {
+        return { ok: false, error: 'Dish not found' };
+      }
+      if (dish.restaurantId !== restaurantId) {
+        return { ok: false, error: 'Dish is not from this restaurant' };
+      }
+      await this.orderItems.save(
+        this.orderItems.create({ dish, options: item.options }),
+      );
+    });
   }
 }
