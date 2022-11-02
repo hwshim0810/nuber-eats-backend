2673f612099acf5535ca656aa6976cefdd148f2b
Hyunwoo Shim <shim.hw0810@gmail.com>
2022-11-02 23:08:39 +0900

    Add schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -72,6 +73,7 @@ import { Payment } from './payments/entities/payment.entity';
         };
       },
     }),
+    ScheduleModule.forRoot(),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
